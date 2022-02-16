4db7fb51c0c8c299335034dc20cc745885c9d5ae
Aurélio Silva de Jesus <aurelin95@gmail.com>
2022-02-15 21:19:14 -0300

    finishing a little resource of get repository by api github

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { RepositoryList } from './components/RepositoryList';
+import './styles/global.scss';
+
 export function App() {
   
-  return <h1>Hello React Js!</h1>
+  return (
+    <>
+      <RepositoryList />
+    </>
+  )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+isDevelopment = process.env.NODE_ENV !== 'production'; // variáveis de ambiente para iniciar um ambiente, produção eu desenvolvimento cross-env
 
 module.exports = {
 
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map', // facilitar no debug do código pelo navegador
 
   // arquivo de entrada do código que será convertido
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
@@ -22,14 +27,16 @@ module.exports = {
   // webpack-dev-server é um plugin que precisa ser instalado e serve para monitorar as configurações da nossa aplicação
   devServer: {
     static: path.resolve(__dirname, 'public'), // onde fica o conteúdo estático da aplicação
+    hot: true
   },
 
   // plugin para injetar o arquivo de saída dentro do HTML automaticamente
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(), // plugin para dar refresh na página e manter os estados dos componentes
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
-  ],
+  ].filter(Boolean), // caso não esteja em modo de desenvolvimento, o webpack não implicará com o valor "false"
 
   // a forma como a aplicação vai se comportar quando importarmos cada um dos tipos de arquivos
   module: {
@@ -38,8 +45,20 @@ module.exports = {
         // testar para ver se esse arquivo é um arquivo javascript ou não, sem levar em consideração a node modules
         test: /\.js?x$/,
         exclude: /node_modules/,
-        use: 'babel-loader' // faz a integração do babel com o webpack
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        } // faz a integração do babel com o webpack
       },
+      {
+        test: /\.scss/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
   }
 }
